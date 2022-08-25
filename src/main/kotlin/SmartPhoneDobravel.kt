2a65a76d2d0e7354dcc0a3c993791be50853dbe5
Igor Garcia <igorgarciaco@gmail.com>
2022-08-25 19:42:46 -0300

    Smartphone Foldable

--- src/main/kotlin/SmartPhoneDobravel.kt
+++ src/main/kotlin/SmartPhoneDobravel.kt
@@ -1,5 +1,5 @@
-class Phone(var isScreenLightOn: Boolean = false){
-    fun switchOn() {
+open class Phone(var isScreenLightOn: Boolean = false){
+    open fun switchOn() {
         isScreenLightOn = true
     }
 
@@ -11,4 +11,24 @@ class Phone(var isScreenLightOn: Boolean = false){
         val phoneScreenLight = if(isScreenLightOn) "on" else "off"
         println("The phone screen's light is $phoneScreenLight.")
     }
+}
+
+class SmartPhoneDobravel(var isFolded:Boolean = ture): Phone() {
+    override fun switchOn(){
+        if(!isFolded){
+            isScreenLightOn = true
+        }
+    }
+
+    fun fold(){
+        isFolded = true
+    }
+
+    fun unfold(){
+        isFolded = false
+    }
+}
+
+fun main() {
+    
 }
